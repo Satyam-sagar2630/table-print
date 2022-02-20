bd903c68f91ca93232521afaa85649523d913c67
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 00:52:47 +0530

    Add files via upload

---
+++ print table/method table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+
+        static void Main(string[]arg)
+        {
+             int i, n, t ;
+             Console.WriteLine("enter a number :");
+             n = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Table is :");
+             for(i = 1; i <= 10; i++)
+             {
+                    t = n * i;
+                    Console.WriteLine(t);
+                
+             }
+
+        }
+    }
+}
